ee6e7c5aef5ca3968b834c2806592c3ff98c07c0
Takumi Neda <t@naninuneda.com>
2016-05-03 02:52:52 +0900

    add Decoder and Encoder

---
+++ src/com/naninuneda/kirifuda/netty/KirifudaDecoder.java
@@ -0,0 +1,16 @@
+package com.naninuneda.kirifuda.netty;
+
+import java.util.List;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+public class KirifudaDecoder extends ByteToMessageDecoder{
+
+	@Override
+	protected void decode(ChannelHandlerContext ctx, ByteBuf buffer, List<Object> arg2) throws Exception {
+		
+	}
+
+}
---
+++ src/com/naninuneda/kirifuda/netty/KirifudaEncoder.java
@@ -0,0 +1,16 @@
+package com.naninuneda.kirifuda.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class KirifudaEncoder extends MessageToByteEncoder<ClientInfo> {
+
+	@Override
+	protected void encode(ChannelHandlerContext ctx, ClientInfo info, ByteBuf buffer) throws Exception {
+
+
+
+	}
+
+}
